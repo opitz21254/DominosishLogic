f1f6a5012d677e3c73c0864f54ef099adfa24bf6
Opitz <opitz@Opitzs-MacBook-Air.local>
2024-11-14 02:07:58 -0700

    Wrote The Tile Object + Test

--- DominosishTests/UnitTest1.cs
+++ DominosishTests/UnitTest1.cs
@@ -3,8 +3,10 @@ namespace DominosishTests;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void OppositeTilesAreEqual()
     {
-
+        var t1 = new Tile(1, 2);
+        var t2 = new Tile(2, 1);
+        t1.equals(t2).Should().BeTrue();
     }
 }
